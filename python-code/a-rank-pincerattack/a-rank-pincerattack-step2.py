4ef42fa763e623b4a6038f818f84b1d6db200ecb
Ilekaede <asakuramotiyo@gmail.com>
2024-05-16 11:23:45 +0900

    paiza 深さ優先探索2

--- python-code/a-rank-pincerattack/a-rank-pincerattack-step2.py
+++ python-code/a-rank-pincerattack/a-rank-pincerattack-step2.py
@@ -2,41 +2,43 @@ H, W, Y, X = map(int, input().split())
 S = [list(input()) for _ in range(H)]
 
 S[Y][X] = '*'
-def dfs(y, x) :
-    dy = y + 1
-    if dy >= H:
-        return
-    else:
-        dfs(dy, x)
-        if S[dy][x] == '*':
-            S[y][x] = '*'
-        
-    dy = y - 1
-    if dy < 0:
-        return
-    else:
-        dfs(dy, x)
-        if S[dy][x] == '*':
-            S[y][x] = '*'
-        
-    
-    dx = x + 1
-    if dx >= W:
-        return
-    else:
-        dfs(y, dx)
-        if S[y][dx] == '*':
-            S[y][x] = '*'
-    
-    dx = x - 1
-    if dx < 0:
-        return
-    else:
-        dfs(y, dx)
-        if S[y][dx] == '*':
-            S[y][x] = '*'
 
-dfs(Y, X)
+def reverse(dy, dx, dest):
+    global flag
+    if not(0 <= dx < W and 0 <= dy < H):
+        return
+    if S[dy][dx] == '.': # 1つ先にコマが置いてないパターン
+        if dest == 'N':
+            reverse(dy - 1, dx, dest)
+        elif dest == 'S':
+            reverse(dy + 1, dx, dest)
+        elif dest == 'W':
+            reverse(dy, dx - 1, dest)
+        elif dest == 'E':
+            reverse(dy, dx + 1, dest)
+        if flag:
+            # print('change * is done')
+            S[dy][dx] = '*'
+        return
+    else:
+        flag = True
+        return
+
+flag = False
+dest = 'N'
+reverse(Y - 1, X, dest)
+
+flag = False
+dest = 'S'
+reverse(Y + 1, X, dest)
+
+flag = False
+dest = 'W'
+reverse(Y, X - 1, dest)
+
+flag = False
+dest = 'E'
+reverse(Y, X + 1, dest)
 
 for i in range(H):
     print(''.join(S[i]))
